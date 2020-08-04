166fc154bfe719af9eef88263c4de64fbeea4800
Usman <usman1607@yahoo.com>
2020-08-04 15:42:28 +0100

    This is a new change

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,5 +1,6 @@
 class SimpleCalculator {
 	public static void main(String[] args){
 		System.out.println("This is a simple calculator written in JAVA...");
+		System.out.println("I am now making some progress...");
 	}
 }
