2fc368718b6650db38483d302328db406b38113d
Usman <usman1607@yahoo.com>
2020-08-05 12:54:11 +0100

    Day thire commitment

---
+++ House.java
@@ -0,0 +1,5 @@
+class House {
+	public static void main(String[] args){
+		System.out.println("This is my house project written in JAVA...");
+	}
+}
--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -2,5 +2,6 @@ class SimpleCalculator {
 	public static void main(String[] args){
 		System.out.println("This is a simple calculator written in JAVA...");
 		System.out.println("I am now making some progress...");
+		System.out.println("This is third modification");
 	}
 }
